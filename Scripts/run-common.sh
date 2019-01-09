33a42d6de86c22e1b5d042a075ddc9e78ef6d558
Jwata <watanabe0621@gmail.com>
2019-01-09 16:02:43 +0900

    run players

--- Scripts/run-common.sh
+++ Scripts/run-common.sh
@@ -1,3 +1,4 @@
+volumes=$(echo $DOCKER_VOLS)
 
 run_player() {
     bin=$1
@@ -7,19 +8,23 @@ run_player() {
     fi
     params="-f 1 $*"
     rem=$(($players - 2))
-    for i in $(seq 0 $rem); do
-      echo "trying with player $i"
-      >&2 echo Running ../$bin $i $params
-      ./$bin $i $params 2>&1 | tee Scripts/logs/$i  &
-    done
     last_player=$(($players - 1))
-    >&2 echo Running ../$bin $last_player $params
-    ./$bin $last_player $params > Scripts/logs/$last_player 2>&1 || return 1
+    for i in $(seq 0 $last_player); do
+      echo "starting up player $i"
+      docker run --rm -d $volumes --network $DOCKER_NET --ip 172.29.0.10$(($i+1)) --name ${DOCKER_PRE}-player-$i $CONTAINER tail -f /dev/null
+    done
+    # for i in $(seq 0 $rem); do
+    #   echo "trying with player $i"
+    #   >&2 echo Running ../$bin $i $params
+    #   docker exec $docker_pre-player-$i $bin $i $params 2>&1 >Scripts/logs/$i &
+    # done
+    # >&2 echo Running ../$bin $last_player $params
+    # ./$bin $last_player $params > Scripts/logs/$last_player 2>&1 || return 1
 }
 
 killall Player.x 
 sleep 0.5
 
-declare -i players=$(sed -n 2p Data/NetworkData.txt)
+declare -i players=$(docker run --rm -a STDOUT $volumes $CONTAINER cat Data/NetworkData.txt | sed -n 2p)
 
 #. Scripts/setup.sh
--- Scripts/test.sh
+++ Scripts/test.sh
@@ -24,11 +24,13 @@ shift $[OPTIND-1]
 run_test() {
     test=$1
     shift
-    printf "\n\n\n\n\n\n\n\n\n\n"
+    # printf "\n\n\n\n\n\n\n\n\n\n"
     echo "$test"
-    $ROOT/compile.py --dead-code-elimination $compile_opts $* Programs/$test || exit 1
+    # cp Auto-Test-Data/${MPC_SETTING}/* Data/
+    docker run -a STDOUT -a STDERR --rm $(echo $DOCKER_VOLS) ${CONTAINER} \
+      /scale-mamba/compile.py --dead-code-elimination $compile_opts $* Programs/$test || exit 1
     Scripts/run-online.sh Programs/$test || exit 1
-    python Scripts/test-result.py $test_opts $test || exit 1
+    # python Scripts/test-result.py $test_opts $test || exit 1
 }
 
 if test "$1"; then
@@ -39,6 +41,6 @@ else
     test_opts="-s"
     compile_opts="--stop"
     for test in test_all test_array test_branch test_branching test_comparison test_count test_empty_tape test_flex test_float test_floatingpoint test_float_sorting test_float_vector test_for_range_multithread test_function test_idle_threads test_lib test_loop test_map_reduce test_mem_order test_new_threads test_threads test_vector test_sfix test_sqrt test_math test_custom_array; do
-	run_test $test
+      run_test $test
     done
 fi
