4f2ea3cf658b4a62c8e3e3df3d97f321f194c5ea
Jwata <watanabe0621@gmail.com>
2019-01-10 15:41:54 +0900

    dockerize

--- Scripts/run-common.sh
+++ Scripts/run-common.sh
@@ -1,3 +1,4 @@
+volumes=$(echo $DOCKER_VOLS)
 
 run_player() {
     bin=$1
@@ -5,21 +6,45 @@ run_player() {
     if ! test -e Scripts/logs; then
         mkdir Scripts/logs
     fi
-    params="$*"
+    params="-f 1 -v 1 $*"
     rem=$(($players - 2))
+    last_player=$(($players - 1))
+
+    trap 'echo interrupting players' SIGINT
+
+    # run players
+    for i in $(seq 0 $last_player); do
+      echo "starting up player $i"
+      docker run --rm -d $volumes --network $DOCKER_NET --ip 172.29.0.10$(($i+1)) --name ${DOCKER_PRE}-player-$i $CONTAINER tail -f /dev/null
+    done
+
+    # run program on each container
     for i in $(seq 0 $rem); do
       echo "trying with player $i"
       >&2 echo Running ../$bin $i $params
-      ./$bin $i $params 2>&1 | tee Scripts/logs/$i  &
+       docker exec ${DOCKER_PRE}-player-$i $bin $i $params 2>&1 >Scripts/logs/$i &
     done
-    last_player=$(($players - 1))
+    echo "trying with player $last_player"
     >&2 echo Running ../$bin $last_player $params
-    ./$bin $last_player $params > Scripts/logs/$last_player 2>&1 || return 1
+    docker exec ${DOCKER_PRE}-player-$last_player ./$bin $last_player $params 2>&1 >Scripts/logs/$last_player
+    result=$?
+
+    # extract memory data
+    for i in $(seq 0 $last_player); do
+      docker cp ${DOCKER_PRE}-player-$i:/scale-mamba/Data/Memory-P$i Data/Memory-P$i
+    done
+    docker cp ${DOCKER_PRE}-player-0:/scale-mamba/Data/SharingData.txt Data/
+
+    # stop players
+    for i in $(seq 0 $last_player); do
+      docker stop -t 0 ${DOCKER_PRE}-player-$i > /dev/null
+    done
+
+    trap SIGINT
+
+    return $result
 }
 
-killall Player.x 
-sleep 0.5
-
-declare -i players=$(sed -n 2p Data/NetworkData.txt)
+declare -i players=$(docker run --rm -a STDOUT $volumes $CONTAINER cat Data/NetworkData.txt | sed -n 2p)
 
 #. Scripts/setup.sh
--- Scripts/test.sh
+++ Scripts/test.sh
@@ -24,9 +24,9 @@ shift $[OPTIND-1]
 run_test() {
     test=$1
     shift
-    printf "\n\n\n\n\n\n\n\n\n\n"
     echo "$test"
-    $ROOT/compile.py --dead-code-elimination $compile_opts $* Programs/$test || exit 1
+    docker run -a STDOUT -a STDERR --rm $(echo $DOCKER_VOLS) ${CONTAINER} \
+      /scale-mamba/compile.py --dead-code-elimination $compile_opts $* Programs/$test || exit 1
     Scripts/run-online.sh Programs/$test || exit 1
     python Scripts/test-result.py $test_opts $test || exit 1
 }
@@ -39,6 +39,6 @@ else
     test_opts="-s"
     compile_opts="--stop"
     for test in test_all test_array test_branch test_branching test_comparison test_count test_empty_tape test_flex test_float test_floatingpoint test_float_sorting test_float_vector test_for_range_multithread test_function test_idle_threads test_lib test_loop test_map_reduce test_mem_order test_new_threads test_threads test_vector test_sfix test_sqrt test_math test_custom_array; do
-	run_test $test
+      run_test $test
     done
 fi
